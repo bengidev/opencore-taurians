314af4f4a2a510fbee56905bdde6c148817f93a8
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-07-11 13:23:23 +0700

    Rewire shell to center-column layout with animated side slots.

--- src/modules/shell/ui/shellScreen.tsx
+++ src/modules/shell/ui/shellScreen.tsx
@@ -1,9 +1,8 @@
 import { useShellStore } from "../state/shellStore";
-import { ShellBottomPanel } from "./panels/shellBottomPanel";
+import { ShellCenterColumn } from "./shellCenterColumn";
 import { ShellLeftPanel } from "./panels/shellLeftPanel";
-import { ShellMainPanel } from "./panels/shellMainPanel";
+import { ShellPanelSlot } from "./shellPanelSlot";
 import { ShellRightPanel } from "./panels/shellRightPanel";
-import { ShellModeBar } from "./shellModeBar";
 
 export function ShellScreen() {
   const leftVisible = useShellStore((s) => s.leftVisible);
@@ -11,23 +10,15 @@ export function ShellScreen() {
   const leftPanelWidth = useShellStore((s) => s.leftPanelWidth);
   const rightPanelWidth = useShellStore((s) => s.rightPanelWidth);
 
-  const columns = [
-    leftVisible ? `${leftPanelWidth}px` : null,
-    "minmax(0, 1fr)",
-    rightVisible ? `${rightPanelWidth}px` : null,
-  ]
-    .filter(Boolean)
-    .join(" ");
-
   return (
-    <div className="grid h-dvh grid-rows-[auto_1fr_auto] bg-background text-foreground">
-      <ShellModeBar />
-      <div className="grid min-h-0" style={{ gridTemplateColumns: columns }}>
-        {leftVisible ? <ShellLeftPanel /> : null}
-        <ShellMainPanel />
-        {rightVisible ? <ShellRightPanel /> : null}
-      </div>
-      <ShellBottomPanel />
+    <div className="flex h-dvh min-h-0 bg-background text-foreground">
+      <ShellPanelSlot visible={leftVisible} width={leftPanelWidth}>
+        <ShellLeftPanel />
+      </ShellPanelSlot>
+      <ShellCenterColumn />
+      <ShellPanelSlot visible={rightVisible} width={rightPanelWidth}>
+        <ShellRightPanel />
+      </ShellPanelSlot>
     </div>
   );
 }
