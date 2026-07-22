6b5fd5476fee3913629a5a50114d11f0b18df270
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-07-22 20:59:57 +0700

    Add editor external read and path probe Desktop commands.

--- src-tauri/src/editor/mod.rs
+++ src-tauri/src/editor/mod.rs
@@ -1,8 +1,12 @@
 pub mod create;
 mod error;
+pub mod path_query;
 pub mod read;
+pub mod read_external;
 pub mod write;
 
 pub use create::editor_create_file;
+pub use path_query::{editor_is_under_root, editor_paths_include_directory};
 pub use read::editor_read_file;
+pub use read_external::editor_read_external_file;
 pub use write::editor_write_file;
